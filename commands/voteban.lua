ad0dece3a0d5905c9afa635ece1eb7bbe59ac52b
Uppernate <uppernatearts@gmail.com>
2017-05-20 18:26:58 +0100

    Update

--- commands/voteban.lua
+++ commands/voteban.lua
@@ -2,7 +2,7 @@ local file = require("../libs/file.lua")
 
 --[[
   presumed defaults for stuff:
-    minVoteMemeCount is the amount of votes required to ban someone, defaults to 5
+    minVotebanCount is the amount of votes required to ban someone, defaults to 5
     voteBanExpireTime is the time in seconds that a vote will take to expire, defaults to 900 (15 minutes)
 ]]--
 
@@ -24,6 +24,15 @@ local votes = files.votes:toTable()
 ]]--
 
 function main(message, args)
+  --Removing expired votes
+  for memberID, voteTypes in pairs(votes) do
+    for voter, memVote in pairs(voteTypes.ban) do
+      if os.time() - memVote.time >= config.voteBanExpireTime then
+        print("[INFO] The vote to ban a user by the ID of " .. memVote.id .. " has silently expired.")
+        table.remove(voteTypes.ban, voter)
+      end
+    end
+  end
   --Detecting Mentioned Users and turning them into members
   local members = {}
   for users in message.mentionedUsers do
@@ -50,16 +59,30 @@ function main(message, args)
     --Meeting target requirements
     if member.roleCount > 0 then message.channel:sendMessage("You can not voteban members with roles!") break end
     --Starting main
-    if votes[member.id] == nil then votes[member.id] = {} end
-    local entry = votes[member.id]
-    if entry.ban == nil then entry.ban = {} end
-    if entry.ban[message.member.id] == nil then
-      entry.ban[message.member.id] = os.time()
-      local vote = entry.ban[message.member.id]
-      p(vote)
-      message.channel:sendMessage(message.member.username.." has voted to ban "..member.username)
+    if votes[member.id] == nil then
+      votes[member.id] = {}
+      message.channel:sendMessage(message.member.mentionString.." has started a vote to ban "..member.mentionString.."! "..(config.minVotebanCount-1).." more votes are required.")
+    end
+    if votes[member.id].ban == nil then
+      votes[member.id].ban = {}
+    end
+    local unique = true
+    for i, v in pairs(votes[member.id].ban) do if v.id == message.member.id then unique = false break end end
+    if unique then
+      table.insert(votes[member.id].ban, {id=message.member.id, time=os.time()})
+      p(message.member.id, votes[member.id].ban[#votes[member.id].ban], #votes[member.id].ban)
+      message.channel:sendMessage(message.member.mentionString.." has voted to ban "..member.username..". "..(config.minVotebanCount-#votes[member.id].ban).." more votes are required.")
+    else
+      message.channel:sendMessage(message.member.mentionString.." you have already voted for "..member.username)
+    end
+    if #votes[member.id].ban == config.minVotebanCount then
+      member:sendMessage("Unfortunately, you have been votebanned in the Click Converse server! If you feel this ban was in error, please contact a moderator from the server to appeal e.g. Uppernate#2858.")
+      message.channel:sendMessage(member.username.." has been successfully voted to be banned from this server.")
+      print("INFO: "..member.username.." has been votebanned from the server.")
+      member:ban(message.guild)
     end
   end
+  files.votes:saveFromTable(votes)
 end
 
 return main
