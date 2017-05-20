c122b8f8bf5bc0976ed27f804ea3c4a683a6b305
Advaith <advaith1@users.noreply.github.com>
2017-05-19 21:58:21 -0700

    add .shaders
    links to ClickStore Shaders page and Phi's Shader list

---
+++ commands/shaders.lua
@@ -0,0 +1,4 @@
+function main(cmd, message)
+  message.channel:sendMessage("ClickStore Shaders page: http://clickstore.clickteam.com/libraries/shaders\nPhi's Shader List: https://sites.google.com/site/mmf2stuff/shaders")
+end
+return main
