1fb29ef01ec85508880dca9e19a6e8b959dfe034
Uppernate <uppernatearts@gmail.com>
2017-05-20 20:35:11 +0100

    Update & Comments

--- clickbot.lua
+++ clickbot.lua
@@ -7,50 +7,63 @@
 ////////////////////////////////////////////////////
 ]]
 
-local discordia = require('discordia')
+-- Important libraries
+local discordia = require('discordia') -- Cool Discord API library
 local client = discordia.Client()
-local ticker = require("./libs/ticker.lua")
-local file = require("./libs/file.lua")
-local cutter = require("./libs/cutter.lua")
+local ticker = require("./libs/ticker.lua") -- Cool timer thing I wrote which I will probably never use
+local file = require("./libs/file.lua") -- Cool file reader and lua table converter thing I wrote
+local cutter = require("./libs/cutter.lua") -- Cool delimiter thing I wrote
 
---Files
+-- Files
 local files = {
   config = file.load("./UppBot/data/config.txt"),
   commands = file.load("./UppBot/data/commands.txt"),
   memberRoles = file.load("./UppBot/data/memberInfo.txt")
 }
 
---File Tables
+-- File Tables
 local config = files.config:toTable()
 local commands = files.commands:toTable()
 local memberRoles = files.memberRoles:toTable()
 
+-- Loading in command .lua files
 for i, v in pairs(commands) do
   v.main = require("./commands/"..i..".lua")
 end
 
 client:on("ready", function()
-  print('Logged in as '.. client.user.username)
+  print("Logged in as " .. client.user.username)
   client:setGameName("type .help for my list of commands")
 end)
 
 function commandExecute(message, command, args)
+
+  -- Unknown command
   if commands[command] == nil then
     p(message.author.username.." ATTEMPTED", command, args)
-    --message.channel:sendMessage(message.author.mentionString..", this command does not exist.")
+    -- message.channel:sendMessage(message.author.mentionString..", this command does not exist.")
     return
   end
+
+  -- Failsafes, if guild commands get executed on private channels, things can explode.
   if message.guild == nil and commands[command].type == "guild" then return end
   if message.guild ~= nil and commands[command].type == "priv" then return end
+
+  -- Known command, executing
+  -- TODO: Role requirements
+
   if commands[command].main ~= nil then
     p(message.author.username.." ACCESSED", command, args)
     commands[command].main(message, args)
   else
+    -- Known command but .lua file in commands folder does not exist.
     p(message.author.username.." ACCESSED BUT FAILED", command, args)
   end
 end
 
+-- Main thing
 client:on("messageCreate", function(message)
+  -- Getting first word
   local nearest_space = message.content:find(" ")
   local command = nil
   if nearest_space ~= nil then
@@ -58,18 +71,31 @@ client:on("messageCreate", function(message)
   else
     command = message.content:lower()
   end
-  --Checking if the first word was prefixed
+  -- Checking if the first word was prefixed
   local commandPrefixed = nil
-  for i, v in pairs(config.prefixes) do
+  for i, v in pairs(config.prefixes) do -- Looping through prefixes
     local result = command:sub(1, #v)
-    if result == v then commandPrefixed = v command = command:sub(#v+1, #command) break end
+    if result == v then -- Found prefix used.
+      commandPrefixed = v
+      command = command:sub(#v+1, #command)
+      break
+    end
   end
+  -- If a prefix was used then...
   if commandPrefixed ~= nil then
-    local cmdContent = cutter.cut(message.content, "%p%s+")
-    cmdContent[1] = cmdContent[1]:sub(#command+#commandPrefixed+2, #cmdContent[1])
-    commandExecute(message, command, cmdContent)
+    local cmdContent = cutter.cut(message.content, "%p%s+") -- Cutting rest of the text using ", " and then making them arguments
+    cmdContent[1] = cmdContent[1]:sub(#command+#commandPrefixed+2, #cmdContent[1]) -- Cutting the first word from the first argument
+    if cmdContent[1] == "" then cmdContent[1] = nil end -- When the first argument is "", remove it completely.
+    commandExecute(message, command, cmdContent) -- Forwarding the info to the function
   end
+
+  -- Do nothing if no prefix was used ¯\_(ツ)_/¯
 end)
+
+-- Reassigning roles
+-- Credits:
+-- Idea and main code: Lumez
+-- Fixes: Uppernate
 
 client:on("memberJoin", function(member)
   if memberRoles[member.id] ~= nil then
@@ -90,6 +116,8 @@ client:on("memberLeave", function(member)
   end
   files.memberRoles:saveFromTable(memberRoles)
 end)
+
+-- Reassigning roles end
 
 p("Click Bot running")
 client:run(config.token)
---
+++ commands/devroom.lua
@@ -0,0 +1,5 @@
+function main(message, args)
+  message.member:addRoles(message.guild:getRole("name", "Clickbot Devs"))
+end
+
+return main
