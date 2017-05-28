995017417e7c5f78d7d558b3158a904e75a16bc3
Uppernate <uppernatearts@gmail.com>
2017-05-28 20:12:06 +0100

    Accidentally changed config command to _config

--- addons/guildSetup.lua
+++ addons/guildSetup.lua
@@ -7,9 +7,9 @@ function gSetup.main(pack)
   local files = pack.files
   local database = files.database.table
   local guild = pack.guild
-  database[guild.id] = database[guild.id] or {name=guild.name, addons={_config={}}, commands={_config={addon="_config", name="main"}}, new=true}
+  database[guild.id] = database[guild.id] or {name=guild.name, addons={_config={}}, commands={config={addon="_config", name="main"}}, new=true}
   database[guild.id].addons = database[guild.id].addons or {_config={}}
-  database[guild.id].commands = database[guild.id].commands or {_config={addon="_config", name="main"}}
+  database[guild.id].commands = database[guild.id].commands or {config={addon="_config", name="main"}}
   if database[guild.id].new then
     print("New Guild: "..guild.name)
     database[guild.id].new = nil
