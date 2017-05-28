4a8041743f4641bb93409401910691c8086d37cc
Uppernate <uppernatearts@gmail.com>
2017-05-28 01:12:09 +0100

    Init: Removed unnecessary steps

--- init.lua
+++ init.lua
@@ -12,8 +12,6 @@ local files = {
 
 if files.config.table == nil then
   files.config.table = {
-    versionURL="https://cdn.rawgit.com/Uppernate/UppBot/master/data/version.txt",
-    version="0",
     prefixes={
       "."
     },
@@ -30,55 +28,26 @@ end
 --File Tables
 local config = files.config:toTable()
 
-print("Version: "..config.version)
-print("Checking updates...")
-
-local fetchedVersion = ""
-
-function init4() --Running
+function init3() --Running
   require("./"..config.toRun..".lua")
 end
 
-function init3() --Updating
+function init2() -- Git check update and add onto update
   local handle = io.popen("cd " .. config.absoluteDir .. " \n git pull")
   local result = handle:read("*a")
   handle:close()
   print(result)
-  init4() --I don't know how to make updating for now, welp
-end
-
-function init2() --Checking if to update
-  fetchedVersion = tonumber(fetchedVersion)
-  --p("Latest version is '"..fetchedVersion.."', current version is '"..config.version.."'")
-  if fetchedVersion ~= nil and fetchedVersion > tonumber(config.version) then
-    local behind = fetchedVersion - tonumber(config.version)
-    local text = ""
-    if behind == 1 then text = "An update behind, " else text = behind.." updates behind, " end
-    print(text.."getting latest update...")
-    init3()
-  elseif fetchedVersion ~= nil and fetchedVersion <= tonumber(config.version) then
-    print("Up to date.")
-    init4()
-  elseif fetchedVersion == nil then
-    print("Error: Was not able to get latest version, skipping...")
-    init4()
-  end
-end
-
-function getVersion()
-  http.new({
-    url = config.versionURL,
-    data = function(chunk) fetchedVersion = fetchedVersion..chunk end,
-  ended = function() init2() end
-  })
+  init3() -- Run bot
 end
 
 if config.update == "true" then
+  print("Checking updates...")
   print("If you don't want to check for updates, change 'update' in data/config.txt to false.")
-  getVersion()
+  init2()
   config.update = "true-nonotify"
 elseif config.update == "true-nonotify" then
-  getVersion()
+  print("Checking updates...")
+  init2()
 elseif config.update == "false" then
   print("Updates are disabled, if you want them again change 'update' in data/config.txt to true")
   config.update = "false-nonotify"
