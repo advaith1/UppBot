447cc2e934b76bf4b9cd986b460e94485a75cb8e
Uppernate <uppernatearts@gmail.com>
2017-06-12 17:47:51 +0100

    Update

---
+++ addons/fusionLinks.lua
@@ -0,0 +1,28 @@
+local basic = {}
+basic.name = "fusionLinks"
+basic.desc = "Adds a few commands to links for Fusion"
+
+function basic.event_added(pack)
+  local message = pack.message
+  local files = pack.files
+  local dataGuild = files.database.table[message.guild.id]
+  if not dataGuild.commands.extlist then
+    dataGuild.commands.extlist = {addon="fusionLinks", name="extlist"}
+  end
+end
+
+function basic.event_removed(pack)
+  local message = pack.message
+  local files = pack.files
+  local dataGuild = files.database.table[message.guild.id]
+  if dataGuild.commands.extlist and dataGuild.commands.extlist.addon == "fusionLinks" then
+    dataGuild.commands.extlist = nil
+  end
+end
+
+function basic.cmd_extlist(pack)
+  local message = pack.message
+  message.channel:sendMessage("Darkwire Extension List: \\https://dark-wire.com/store/extlist.php\nOfficial Clickteam Extension Manager List: \\https://www.clickteam.com/cem\nClickWiki Extension List: \\http://clickwiki.net/wiki/Extensions#Extension_List")
+end
+
+return basic
--- bot.lua
+++ bot.lua
@@ -11,17 +11,16 @@ local files = {
 }
 if files.database.table == nil then files.database.table = {} end -- New database is made if one doesn't exist
 
-local master = "143784331339497473" -- Master's ID
-
 -- Addons:
 local addons = { } -- Addons with _ at the beginning of their name are global addons and they cannot be added or removed from a server.
 addons._guildSetup = addon.new("guildSetup")
 addons._commands = addon.new("commands")
 addons._permissions = addon.new("permissions")
 addons._config = addon.new("config")
-addons.basic = addon.new("basic")
+addons.ping = addon.new("basic")
 addons.rolesStay = addon.new("rolesStay")
 addons.help = addon.new("help")
+addons.fusionLinks = addon.new("fusionLinks")
 
 -- For fetching stuff in here, just in case:
 function get(p)
