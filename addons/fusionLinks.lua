88882214ea211478adcb2c18b64d8a139c50f6e9
Advaith <advaith1@users.noreply.github.com>
2017-06-23 16:08:57 -0700

    add shaders

--- addons/fusionLinks.lua
+++ addons/fusionLinks.lua
@@ -7,8 +7,10 @@ function basic.event_added(pack)
   local files = pack.files
   local dataGuild = files.database.table[message.guild.id]
   local cmdName = "extlist"
+  local cmdName = "shaders"
   if not dataGuild.commands[cmdName] then
     dataGuild.commands[cmdName] = {addon="fusionLinks", name="extlist"}
+    dataGuild.commands[cmdName] = {addon="fusionLinks", name="shaders"}
   end
 end
 
@@ -17,6 +19,7 @@ function basic.event_removed(pack)
   local files = pack.files
   local dataGuild = files.database.table[message.guild.id]
   local cmdName = "extlist"
+  local cmdName = "shaders"
   if dataGuild.commands[cmdName] and dataGuild.commands[cmdName].addon == "fusionLinks" then
     dataGuild.commands[cmdName] = nil
   end
@@ -25,6 +28,11 @@ end
 function basic.cmd_extlist(pack)
   local message = pack.message
   message.channel:sendMessage("Darkwire Extension List: https://dark-wire.com/store/extlist.php\nOfficial Clickteam Extension Manager List: https://www.clickteam.com/cem\nClickWiki Extension List: https://clickwiki.net/wiki/Extensions#Extension_List")
+end
+
+function basic.cmd_shaders(pack)
+  local message = pack.message
+  message.channel:sendMessage("ClickStore Shaders page: http://clickstore.clickteam.com/libraries/shaders\nPhi's Shader List: https://sites.google.com/site/mmf2stuff/shaders")
 end
 
 return basic
